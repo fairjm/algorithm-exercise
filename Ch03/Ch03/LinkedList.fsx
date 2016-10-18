008ae92c32c306d48b427322791efe10f76fe5c7
yukin <fairjm@outlook.com>
2016-10-18 22:58:26 +0800

    begin ch03.start do some coding

---
+++ Ch03/Ch03/LinkedList.fsx
@@ -0,0 +1,21 @@
+﻿type Cell<'t>(t : 't, next : Cell<'t> option) =
+    new(t : 't) = new Cell<'t>(t, None)
+    new(t:'t, next : Cell<'t>) = new Cell<'t>(t, Some(next))
+    member val Value = t with get, set
+    member val Next = next with get, set
+
+module Cell =
+
+    let print<'t> (top : Cell<'t>) = 
+        let rec printInner (cellOpt : Cell<'t> option) = 
+            cellOpt |> Option.bind (fun e -> 
+                           printfn "%A" e.Value
+                           printInner e.Next)
+        printInner (Some(top))
+    
+    let find<'t when 't : equality> (value : 't) (top : Cell<'t>) = 
+        let rec findInner (cellOpt : Cell<'t> option, value : 't) = 
+            cellOpt |> Option.bind (fun e -> 
+                           if value = e.Value then cellOpt
+                           else findInner (e.Next, value))
+        findInner (Some(top), value)
---
+++ Ch03/Ch03/Script.fsx
@@ -0,0 +1,11 @@
+﻿#load "LinkedList.fsx"
+
+open LinkedList
+open LinkedList.Cell
+
+let cell = new Cell<int>(1)
+let cell2 = new Cell<int>(2)
+cell.Next <- Some(cell2)
+
+cell |> Cell.print
+
